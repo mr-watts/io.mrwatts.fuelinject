c9488dd7738be359771ab44bfb0d1d42e8cb558d
NoTuxNoBux <tux@mrwatts.io>
2023-07-25 15:56:19 +0200

    CI: Report dotnet build analyzer errors

---
+++ ci/dotnet-build-output-to-junit-json.py
@@ -0,0 +1,96 @@
+#!/bin/env python3
+#
+
+import re
+import json
+import time
+import fileinput
+
+expression = re.compile('(?P<file>.+)\((?P<line>\d+),(?P<column>\d+)\): (?P<severity>error|warning) (?:(?P<analyzerCode>\w+): )?(?P<message>.+)\[(?P<csprojPath>.+)\]')
+
+tests_by_file = {}
+
+for line in fileinput.input():
+    matches = expression.match(line)
+
+    if matches is None:
+        continue;
+
+    file = matches.group('file')
+
+    if file not in tests_by_file:
+        tests_by_file[file] = []
+
+    tests_by_file[file].append({
+        "title": matches.group('file'),
+        "duration": 0,
+        "tests": [
+            {
+                "title": matches.group('analyzerCode') or 'Unknown',
+                "state": "failed",
+                "fail": True,
+                "duration": 0,
+                "err": {
+                    "message": f"{matches.group('severity').upper()}: {matches.group('line')}:{matches.group('column')}: {matches.group('message').strip()}"
+                }
+            }
+        ]
+    })
+
+suites = []
+
+for file, tests in tests_by_file.items():
+    suites.append({
+        "title": file,
+        "tests": tests
+    });
+
+print(json.dumps({
+    "reportTitle": "dotnet build",
+    "stats": {
+        # "start": datetime.datetime.now(datetime.timezone.utc).strftime('%d/%m/%Y %H:%M:%S.%f')
+        "start": time.time()
+    },
+    "suites": {
+        "suites": suites
+    }
+}))
+
+# {
+#   "reportTitle": "dotnet format",
+#   "stats": {
+#     "start": 1690290890.572147
+#   },
+#   "suites": {
+#     "suites": [
+#       {
+#         "title": "/home/werk/Projecten/Internal/Packages/unity-fuel-inject/Assets/Scripts/Services/MonoBehaviourWithGameObjectInstantiatorDependency.cs",
+#         "tests": [
+#           {
+#             "title": "MonoBehaviourWithGameObjectInstantiatorDependency.cs (WHITESPACE)",
+#             "state": "failed",
+#             "fail": true,
+#             "duration": 0,
+#             "err": {
+#               "message": "5:90: Fix whitespace formatting. Insert '\\n\\s\\s\\s\\s'."
+#             }
+#           }
+#         ]
+#       },
+#       {
+#         "title": "/home/werk/Projecten/Internal/Packages/unity-fuel-inject/Assets/Scripts/Services/Bar.cs",
+#         "tests": [
+#           {
+#             "title": "Bar.cs (WHITESPACE)",
+#             "state": "failed",
+#             "fail": true,
+#             "duration": 0,
+#             "err": {
+#               "message": "3:35: Fix whitespace formatting. Insert '\\s'.\n3:36: Fix whitespace formatting. Insert '\\s'."
+#             }
+#           }
+#         ]
+#       }
+#     ]
+#   }
+# }
---
+++ ci/dotnet-build.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -x
+set -o pipefail
+
+echo "Running .NET build"
+
+DOTNET_BUILD_LOG="/tmp/dotnet-build.log"
+JUNIT_JSON_OUTPUT_FILE="$UNITY_DIR/dotnet-build-errors-junit.json"
+
+chmod +x ./ci/dotnet-build-output-to-junit-json.py
+
+dotnet build /nologo /clp:NoSummary | tee $DOTNET_BUILD_LOG
+
+DOTNET_BUILD_EXIT_CODE=$?
+
+./ci/dotnet-build-output-to-junit-json.py < $DOTNET_BUILD_LOG > "$JUNIT_JSON_OUTPUT_FILE"
+
+cat "$JUNIT_JSON_OUTPUT_FILE"
+
+npx jsonjunit --json ci --junit ci
+
+rm $DOTNET_BUILD_LOG
+
+exit $DOTNET_BUILD_EXIT_CODE

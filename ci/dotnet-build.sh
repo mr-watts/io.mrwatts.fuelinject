db783e3e6981649aa71f891ec2c2534b61f1ae4e
NoTuxNoBux <tux@mrwatts.io>
2024-09-18 14:27:05 +0200

    CI: Try to fix no C# project files being installed during .NET analyzers run

--- ci/dotnet-build.sh
+++ ci/dotnet-build.sh
@@ -3,6 +3,26 @@
 set -x
 set -o pipefail
 
+echo "Letting Unity generate C# project files"
+
+${UNITY_EXECUTABLE:-xvfb-run --auto-servernum --server-args='-screen 0 640x480x24' unity-editor} \
+  -projectPath "$UNITY_DIR" \
+  -executeMethod TriggerCSharpProjectEditorScript.GenerateCSharpSDKStyleProjects \
+  -logFile /dev/stdout \
+  -batchmode \
+  -nographics \
+  -quit \
+  -debugCodeOptimization
+
+UNITY_EXIT_CODE=$?
+
+if [ $UNITY_EXIT_CODE -eq 0 ]; then
+  echo "Run succeeded, no failures occurred";
+else
+  echo "Unexpected exit code $UNITY_EXIT_CODE";
+  exit 1
+fi
+
 echo "Running .NET build"
 
 DOTNET_BUILD_LOG="/tmp/dotnet-build.log"
@@ -10,7 +30,7 @@ JUNIT_JSON_OUTPUT_FILE="$UNITY_DIR/dotnet-build-errors-junit.json"
 
 chmod +x ./ci/dotnet-build-output-to-junit-json.py
 
-dotnet build /nologo /clp:NoSummary | tee $DOTNET_BUILD_LOG
+dotnet build /nologo /clp:NoSummary ./*.sln | tee $DOTNET_BUILD_LOG
 
 DOTNET_BUILD_EXIT_CODE=$?
 

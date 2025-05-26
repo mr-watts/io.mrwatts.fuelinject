a8c0e02df6c41f14f6ed70421b03cd59b82caca3
NoTuxNoBux <tux@mrwatts.io>
2025-05-26 10:38:08 +0200

    TEST: Fix DontDestroyOnLoad objects not getting cleared between tests
    When all scenes get unloaded for the purposes of testing, we also want
    DontDestroyOnLoad objects to disappear. Ordinarily they should persist
    scene changes but during tests they shouldn't as tests should be
    isolated from one another and each scene test should start with a fully
    clean slate.
    
    This happens for the Unity Netcode NetworkManager among other objects.
    Without this, each scene load in a new test during a subsequent run
    will accumulate more of these NetworkManager objects.

--- Assets/Package/Testing/Scripts/SceneUnloader.cs
+++ Assets/Package/Testing/Scripts/SceneUnloader.cs
@@ -36,6 +36,32 @@ namespace MrWatts.Internal.FuelInject.Testing
 
                 yield return Unload(activeScene);
             }
+
+            /*
+                When all scenes get unloaded for the purposes of testing, we also want DontDestroyOnLoad objects to
+                disappear. Ordinarily they should persist scene changes but during tests they shouldn't as tests should
+                be isolated from one another and each scene test should start with a fully clean slate.
+            */
+            Array.ForEach(GetDontDestroyOnLoadGameObjects(), UnityEngine.Object.DestroyImmediate);
+        }
+
+        /// <summary>
+        /// Retrieves all GameObjects that are DontDestroyOnLoad, which is shown as a separate scene, but isn't an
+        /// actual scene that is returned in the list of scenes nor are these objects retrievable by ordinary means.
+        /// </summary>
+        /// <remarks>
+        /// See also https://discussions.unity.com/t/editor-script-how-to-access-objects-under-dontdestroyonload-while-in-play-mode/646469/7
+        /// </remarks>
+        private static GameObject[] GetDontDestroyOnLoadGameObjects()
+        {
+            GameObject? dummy = new();
+            UnityEngine.Object.DontDestroyOnLoad(dummy);
+
+            Scene dontDestroyOnLoadScene = dummy.scene;
+
+            UnityEngine.Object.DestroyImmediate(dummy);
+
+            return dontDestroyOnLoadScene.GetRootGameObjects();
         }
     }
 }
